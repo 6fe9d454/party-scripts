10874ea41b709f9815e76c16105a46358156f901
6fe9d454 <spamham@sent.com>
2022-06-02 16:55:05 -0400

    Replace kemono script with generic script

--- party_get_user.py
+++ party_get_user.py
@@ -1,16 +1,30 @@
+"""
+Pull all of the links/attachments from posts for a particular kemono/coomer party user.
+
+Keep note, that the links pulled from the posts, especially with link discovery on, will most likely require post processing on your own part. Unfortunately, due to the random nature of the different services people use and formatting, sometimes errant text makes its way into links (which may end up doubling the links).
+"""
+
+
 import argparse
 import re
 from urllib.parse import urlparse
 
 import requests
+from bs4 import BeautifulSoup
 from lxml import html
+
+PARTY_REGEX = re.compile(
+    r"(?:beta\.)?(?P<party>coomer|kemono)\.party\/(?P<service>.*)\/user\/(?P<user>.*)$",
+    re.IGNORECASE | re.DOTALL,
+)
+LINK_REGEX = re.compile(r"(?P<url>https?://[^\s]+)")
 
 
 def main(args):
 
     links = args.LINKS
     aria_format = args.aria2_format
-    link_conversion = args.link_conversion
+    link_discovery = args.link_discovery
     start_page = args.start_page * 25
     end_page = args.end_page
     if end_page is not None:
@@ -18,28 +32,31 @@ def main(args):
         if start_page > end_page:
             raise SystemExit("Start page is beyond end page!")
 
-    def get_title(link):
-        title = html.fromstring(requests.get(link).content).xpath("//title/text()")[0]
+    def get_title(service, party, user):
+        title = html.fromstring(
+            requests.get(f"https://{party}.party/{service}/user/{user}").content
+        ).xpath("//title/text()")[0]
         return re.findall(r"^Posts of (.*) from .*$", title, re.IGNORECASE | re.DOTALL)[
             0
         ]
 
-    def get_hrefs(contents):
+    def get_links(contents):
         if contents.strip():
-            return html.fromstring(contents).xpath("//a/@href")
+            # Sometimes we get lucky and can just get all of the links easily via xpath since it's HTML
+            # Other times we can't, and it's plain text which is obnoxious
+            links = set(html.fromstring(contents).xpath("//a/@href"))
+            clean_contents = BeautifulSoup(contents.strip(), "lxml").text
+            links.update(LINK_REGEX.findall(clean_contents.strip()))
+            return list(links)
         return []
 
     for link in links:
 
-        service, _, user = (
-            link.replace("https://kemono.party/", "")
-            .replace("http://kemono.party/", "")
-            .split("/")
-        )
-        prefix = f"[{service}, {user}]"
+        party, service, user = PARTY_REGEX.search(link).groups()
+        prefix = f"[{party}, {service}, {user}]"
         print(f"{prefix} Pulling pages ...")
 
-        title = get_title(link).replace(" ", "_").strip()
+        title = get_title(service, party, user).replace(" ", "_").strip()
         output_prefix = f"{title}_{user}_{service}"
         total_links = 0
         total_attachments = 0
@@ -52,7 +69,7 @@ def main(args):
             lprefix = f"{prefix} [{current_page}/{lep}]"
             print(f"{lprefix} Fetching page {current_page} ...")
             posts = requests.get(
-                f"https://kemono.party/api/{service}/user/{user}", params={"o": o}
+                f"https://{party}.party/api/{service}/user/{user}", params={"o": o}
             ).json()
 
             # Reached end
@@ -71,10 +88,14 @@ def main(args):
             post_links = []
             attachments = []
             d_count = 0
+            c_count = 0
             for post in posts:
 
                 # Collect links from text content, usually links to mega, etc
-                post_links.extend(get_hrefs(post["content"]))
+                if link_discovery:
+                    contents_links = get_links(post["content"])
+                    c_count += len(contents_links)
+                    post_links.extend(contents_links)
 
                 # Collect attachments, depending on format will be dumped differently
                 if post["attachments"]:
@@ -82,20 +103,19 @@ def main(args):
                         for attachment in post["attachments"]:
                             name, path = attachment["name"], attachment["path"]
                             attachments.append(
-                                f"https://kemono.party{path}\n out={name}"
+                                f"https://{party}.party{path}\n out={name}"
                             )
                     else:
                         for attachment in post["attachments"]:
                             name, path = attachment["name"], attachment["path"]
-                            attachments.append(f"https://kemono.party{path}")
+                            attachments.append(f"https://{party}.party{path}")
 
                 # Check links if applicable
-                if link_conversion:
-
-                    d_link = None
+                if link_discovery:
 
                     for attachment in post["attachments"]:
 
+                        d_link = None
                         name = attachment["name"]
                         parsed = urlparse(name)
                         parsed_name = parsed.path.lstrip("/")
@@ -123,6 +143,9 @@ def main(args):
                             d_count += 1
 
             print(f"{lprefix} Added an additional {d_count} link(s) from filenames ...")
+            print(
+                f"{lprefix} Added an additional {c_count} link(s) from post contents ..."
+            )
 
             # Append to file(s)
             if post_links:
@@ -143,19 +166,21 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("LINKS", nargs="+")
     parser.add_argument(
+        "-a",
         "--aria2-format",
         default=False,
         action="store_true",
         help="store attachment links with filename in the format required for aria2, otherwise just dumps links as is",
     )
     parser.add_argument(
-        "--link-conversion",
+        "-l",
+        "--link-discovery",
         default=False,
         action="store_true",
-        help="optionally add links born from file name; sometimes the filename is the true, full size image or video (with the link in the attachments being a preview image or thumbnail), may or may not work, assumings videos from imgur are mp4",
+        help="add links born from attachment filename(s) and post contents",
     )
     parser.add_argument("-s", "--start-page", default=0, type=int, help="start page")
     parser.add_argument("-e", "--end-page", default=None, type=int, help="end page")
